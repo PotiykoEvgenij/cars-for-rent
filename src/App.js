37fbbd7464d7f6a5647072b3e1afcddf831ec077
PotiykoEvgenij <potijkoevgenij@gmail.com>
2023-10-09 23:23:18 +0300

    deploy

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      test
-    </div>
-  );
+  return <div className="App">test1</div>;
 }
 
 export default App;
