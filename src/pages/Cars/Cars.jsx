c26553967820fc1390ff124f0c2dcd5e70427223
PotiykoEvgenij <potijkoevgenij@gmail.com>
2023-10-14 20:50:50 +0300

    update styles

--- src/pages/Cars/Cars.jsx
+++ src/pages/Cars/Cars.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import CarsCard from "../../components/carsCard/CarsCard";
+import styles from "./Cars.module.css"
 
 
 export default function Cars() {
     return (
-        <div>
+        <div className={styles.wrapper}>
             <div>Cars</div>
             <CarsCard />
         </div>
