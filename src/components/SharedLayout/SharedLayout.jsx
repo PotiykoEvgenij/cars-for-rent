629400a8202d8477a6d158a680033927d9337fcb
PotiykoEvgenij <potijkoevgenij@gmail.com>
2023-10-14 21:17:53 +0300

    Update SharedLayout.jsx

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -4,14 +4,24 @@ import { NavLink, Outlet } from "react-router-dom";
 import styles from "./SharedLayout.module.css";
 
 export default function SharedLayout() {
-    return (
-        <div>
-            <div className={styles.SharedLayout}>
-                <NavLink to="/" className={styles.NavLink}>Main</NavLink>
-                <NavLink to="/cars" className={styles.NavLink}>Catalog</NavLink>
-                <NavLink to="/favorite" className={styles.NavLink}>Favorite</NavLink>
-            </div>
-            <Outlet></Outlet>
-        </div>
-    );
+  return (
+    <body>
+      <header>
+        <nav className={styles.SharedLayout}>
+          <NavLink to="/" className={styles.NavLink}>
+            Main
+          </NavLink>
+          <NavLink to="/cars" className={styles.NavLink}>
+            Catalog
+          </NavLink>
+          <NavLink to="/favorite" className={styles.NavLink}>
+            Favorite
+          </NavLink>
+        </nav>
+      </header>
+      <main>
+        <Outlet></Outlet>
+      </main>
+    </body>
+  );
 };
